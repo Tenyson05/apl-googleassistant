2e5f0689d4fb1d170a2e1cf2eedfc0f89ee548a6
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-04-01 15:40:33 -0700

    Added lambda calculus and Semantics description  intent

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
-const { dialogflow } = require('actions-on-google');
+const { dialogflow, SimpleResponse } = require('actions-on-google');
+const lamCal = require("lambda-calculus");
 
 const app = dialogflow({
 	debug: true
@@ -9,13 +10,27 @@ Change conv.close to conv.ask
 
 */
 // capture intent 
-app.intent('Group members', (conv) => {
-	conv.conv("APL class of 2019/2020 semester 2 project done by Rojah, Brittany and Stern")
+app.intent('About', (conv) => {
+	conv.ask("This is the APL class of 2019/2020 semester 2 project. These are the fabulous group members. ROjah Lewis, ROhan Stern adn Brittaney Anderson. We welcome you!");
 })
 
-app.intent('What is semantics?', (conv) => {
-	conv.close("Semantics is the process a computer follows when executing a program in a specific language. This is shown by describing the relationship between the input and the output of a program and how it will be executed on a certain platform by creating a model of computation. Three types of semantic include denotational, operational and axiomatic semantic.");
+app.intent('Semantics', (conv) => {
+	conv.ask("Semantics in Programming language design is concenred with the detailed study of the meaning of programming languages. The three types of semantics are operational semantics, denotational semantics and axiomatic semantics. Though each type looks at program in a different manner they all describe and extract the deeper meaning from within a computer program.");
 })
+
+app.intent('Lambda Calculus', (conv, {num}) => {
+	const tet = calcu(num)
+	conv.ask(`Your results are,  ${tet}`)
+})
+app.intent('Quit', (conv) => {
+	conv.close('Leaving so soon? Aw, okay, see ya next time!');
+})
+
+function calcu(testparams) {
+	test1 = lamCal.fromString(testparams);
+	output = lamCal.reduce(test1);
+	return lamCal.toString(output);
+}
 
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
